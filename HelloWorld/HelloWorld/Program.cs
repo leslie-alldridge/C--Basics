7be696508bac82b29dc62ed6ccc1532ae1408705
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-11-03 10:33:14 +1300

    types and logs
    types and logs

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string name = "Leslie";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(name);
+            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            Console.WriteLine("{0} {1}", int.MinValue, int.MaxValue);
+        }
+    }
+}
